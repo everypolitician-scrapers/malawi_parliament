63875aedafd99a6f5be6d8ae868f42b6db03a684
Tony Bowden <tony@tmtm.com>
2015-05-30 18:13:42 +0300

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,71 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/bin/env ruby
+# encoding: utf-8
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'scraperwiki'
+require 'nokogiri'
+require 'date'
+require 'open-uri'
+require 'date'
+require 'csv'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# require 'colorize'
+# require 'pry'
+# require 'csv'
+# require 'open-uri/cached'
+# OpenURI::Cache.cache_path = '.cache'
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read) 
+end
+
+def datefrom(date)
+  Date.parse(date)
+end
+
+def scrape_list(url)
+  warn "Getting #{url}"
+  noko = noko_for(url)
+  noko.css('a[href*="mp.php"]/@href').map(&:text).uniq.each do |rel_link|
+    abs_link = URI.join(url, rel_link)
+    scrape_mp(abs_link)
+  end
+
+  next_page = noko.css('a[href*="mode=alps"]').find { |a| a.text.include? 'Next' }
+  scrape_list(URI.join(url, next_page.attr('href'))) if next_page
+end
+
+def scrape_mp(url)
+  noko = noko_for(url)
+  heading = noko.xpath('.//font[@face="Palatino Linotype"]').first
+  (name, constituency) = heading.text.strip.split(/\s+-\s+/)
+  box = noko.xpath('.//table[contains(.,"Postal addrress")]').last
+  party_info = box.css('a[href*="mode=pp"]').text
+  (party, party_id) = party_info.match(/^(.*)\s\((.*?)\)/).captures rescue (party, party_id) = [party_info, party_info]
+
+  data = { 
+    id: url.to_s[/indexc=(\d+)/, 1],
+    name: name,
+    constituency: constituency,
+    party: party,
+    party_id: party_id,
+    gender: heading.xpath('./following::i').text[/\((.)\)/,1],
+    district: box.xpath('.//td[contains(.,"District")]/following-sibling::td').text.strip,
+    tel: box.xpath('.//td[contains(.,"Phone")]/following-sibling::td').text.strip,
+    # All empty or set to 'None'
+      # address: box.xpath('.//td[contains(.,"Postal addrress")]/following-sibling::td').text.strip,
+    # All empty:
+      # email: box.xpath('.//td[contains(.,"E-mail addrress")]/following-sibling::td').text.strip,
+    term: 2014,
+    source: url.to_s,
+  }
+  ScraperWiki.save_sqlite([:id, :term], data)
+end
+
+term = {
+  id: 2014,
+  name: '2014',
+  start_date: '2014',
+}
+ScraperWiki.save_sqlite([:id], term, 'terms')
+scrape_list('http://www.parliament.gov.mw/mps.php?mode=alps')
+
